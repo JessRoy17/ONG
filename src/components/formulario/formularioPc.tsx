f842c4680731a1d6c59b92a4a7dd8bdeed5d549f
Jess <jessicamiguelroy@gmail.com>
2026-01-08 13:30:13 +0100

    commit opcional

--- src/components/formulario/formularioPc.tsx
+++ src/components/formulario/formularioPc.tsx
@@ -1,45 +1,48 @@
 import './formulario.css'
 
-function formularioPC() {
-    return (
-        <>
-            <form className='margenesForm oculto--formularioPC'>
-                <legend className='legendTitulo'>Voluntariado</legend>
-                    
-                    
-                    <fieldset className='fieldsetForm '>
-                        <input className='placeholderColor' type="text" id="nombre" name="user_nombre" placeholder=' Nombre' />
-                        <input className='placeholderColor' type="text" id="apellido" name="user_apellido" placeholder=' Apellido' />
-                        <input className='placeholderColor' type="text" id="edad" name="user_edad" placeholder=' Edad' />
-                    
-                    </fieldset>
-                   
-                    <fieldset className='fieldsetForm'>
-                        <input className='placeholderColor' type="email" id="mail" name="user_mail" placeholder=' E-mail' />
-                        <input className='placeholderColor' type="text" id="tlf" name="user_tlf" placeholder=' Teléfono' />
-                    </fieldset>
-                    
-                    <fieldset className='fieldsetForm'>
-                        <textarea className='placeholderColor' id="msg" name="motivo" placeholder=' Motivo'></textarea>
-                    </fieldset>
-                    
-                    <fieldset className='fieldsetForm buttonAyudanos'>
-                        <button>Ayúdanos</button>
-                    </fieldset>
-            </form>
+function FormularioPC() {
+  return (
+    <>
+      {/* PC */}
+      <form className="margenesForm formulario oculto--formularioPC">
+        <fieldset>
+          <legend className="legendTitulo">Voluntariado</legend>
 
-            <form className='oculto--formulario'>
-                <legend className='oculto--formulario-legend'>Voluntariado</legend>
-                <input className='placeholderColor' type="text" id="nombre" name="user_nombre" placeholder=' Nombre' />
-                <input className='placeholderColor' type="text" id="apellido" name="user_apellido" placeholder=' Apellido' />
-                <input className='placeholderColor' type="text" id="edad" name="user_edad" placeholder=' Edad' />
-                <input className='placeholderColor' type="email" id="mail" name="user_mail" placeholder=' E-mail' />
-                <input className='placeholderColor' type="text" id="tlf" name="user_tlf" placeholder=' Teléfono' />
-                <textarea className='placeholderColor' id="msg" name="motivo" placeholder=' Motivo'></textarea>
-                <button className='oculto--formulario-button'>Ayúdanos</button>
-            </form>
+          <fieldset className="fieldsetForm">
+            <input className="placeholderColor" type="text" name="user_nombre" placeholder="Nombre" />
+            <input className="placeholderColor" type="text" name="user_apellido" placeholder="Apellido" />
+            <input className="placeholderColor edad" type="text" name="user_edad" placeholder="Edad" />
+          </fieldset>
 
-        </>
-    )
+          <fieldset className="fieldsetForm">
+            <input className="placeholderColor" type="email" name="user_mail" placeholder="E-mail" />
+            <input className="placeholderColor" type="text" name="user_tlf" placeholder="Teléfono" />
+          </fieldset>
+
+          <fieldset className="fieldsetForm">
+            <textarea className="placeholderColor" name="motivo" placeholder="Motivo" />
+          </fieldset>
+
+          <fieldset className="fieldsetForm buttonAyudanos">
+            <button type="submit">Ayúdanos</button>
+          </fieldset>
+        </fieldset>
+      </form>
+
+      {/* Mobile */}
+      <form className="formulario oculto--formulario">
+        <legend className="oculto--formulario-legend">Voluntariado</legend>
+
+        <input className="placeholderColor" type="text" name="user_nombre" placeholder="Nombre" />
+        <input className="placeholderColor" type="text" name="user_apellido" placeholder="Apellido" />
+        <input className="placeholderColor edad" type="text" name="user_edad" placeholder="Edad" />
+        <input className="placeholderColor" type="email" name="user_mail" placeholder="E-mail" />
+        <input className="placeholderColor" type="text" name="user_tlf" placeholder="Teléfono" />
+        <textarea className="placeholderColor" name="motivo" placeholder="Motivo" />
+        <button className="oculto--formulario-button">Ayúdanos</button>
+      </form>
+    </>
+  )
 }
-export default formularioPC
+
+export default FormularioPC
